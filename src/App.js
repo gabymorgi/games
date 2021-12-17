c61571b9f43d82000e7cff0cc2ac99f62972c260
gaby morgi <gabymorgi@gmail.com>
2021-12-16 21:17:05 -0300

    data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import 'antd/dist/antd.css';
-import { Table } from 'antd';
+import { Row, Table } from 'antd';
 //import './index.css'
 import { data } from './data'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
@@ -10,53 +10,59 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 function App() {
   return (
     <div className="App">
-      <Line
-        datasetIdKey='id'
-        data={{
-          labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
-          datasets: [
-            {
-              label: "First dataset",
-              data: [33, 53, 85, 41, 44, 65],
-              fill: true,
-              backgroundColor: "rgba(75,192,192,0.2)",
-              borderColor: "rgba(75,192,192,1)"
-            },
-            {
-              label: "Second dataset",
-              data: [33, 25, 35, 51, 54, 76],
-              fill: false,
-              borderColor: "#742774"
-            }
-          ]
-        }}
-      />
-      <Pie data={{
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [
-          {
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
+      <Row>
+        <Col span={12}>
+          <Line
+            datasetIdKey='id'
+            data={{
+              labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+              datasets: [
+                {
+                  label: "First dataset",
+                  data: [33, 53, 85, 41, 44, 65],
+                  fill: true,
+                  backgroundColor: "rgba(75,192,192,0.2)",
+                  borderColor: "rgba(75,192,192,1)"
+                },
+                {
+                  label: "Second dataset",
+                  data: [33, 25, 35, 51, 54, 76],
+                  fill: false,
+                  borderColor: "#742774"
+                }
+              ]
+            }}
+          />
+        </Col>
+        <Col span={12}>
+          <Pie data={{
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [
+              {
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)',
+                ],
+                borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)',
+                ],
+                borderWidth: 1,
+              },
             ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)',
-            ],
-            borderWidth: 1,
-          },
-        ],
-      }} />
+          }} />
+        </Col>
+      </Row>
       <Table dataSource={data} rowKey="name" size="small" pagination={{
         defaultPageSize: 50,
       }}>
